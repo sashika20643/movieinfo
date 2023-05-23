b811e3927d392f556de46283af1ba01a3d0d2d9d
sashika20643 <sashikadulaj20643@gmail.com>
2023-05-24 01:34:15 +0530

    search api fixed

--- src/components/middlecont.jsx
+++ src/components/middlecont.jsx
@@ -4,9 +4,16 @@ import './css/searchbar.css'
 import Moviecontainer from './moviecontainer'
 const Middlecont = () => {
   const[movie,setmovie]=useState({});
-  
+  const [searchValue, setSearchValue] = useState('');
+  function handleInputChange(event) {
+    setSearchValue(event.target.value);
+  }
+
   const getMovie=async()=>{
-    const url="https://api.themoviedb.org/3/search/movie?api_key=620350bb0173cd80a6e31eeb2b63391c&language=en-US&query=batman&page=1&include_adult=false"
+    
+
+
+    const url="https://api.themoviedb.org/3/search/movie?api_key=620350bb0173cd80a6e31eeb2b63391c&language=en-US&query="+searchValue+"&page=1&include_adult=false"
     const respons= await fetch(url);
     const responsJson=await respons.json();
     setmovie(responsJson.results[0]);
@@ -25,7 +32,7 @@ const Middlecont = () => {
   return (
     <div className='middle'>
 <div>
-        <input type="text" className='search' name="" id="" />
+        <input type="text" className='search'  value={searchValue} onChange={handleInputChange}  name="" id="mnane" />
       
   
       <button className='sbtn' onClick={getMovie} >search</button>
--- src/components/moviecontainer.jsx
+++ src/components/moviecontainer.jsx
@@ -30,3 +30,5 @@ const Moviecontainer = ({movied}) => {
 }
 
 export default Moviecontainer
+
+
